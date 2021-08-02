16d841687a13818c2774511e81203d86bfdf0b96
Karol Hajduk <Karol.hajduk@sabre.com>
2021-08-02 18:16:57 +0200

    Add project files.

---
+++ Models/AppState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BlazorToDoApp.Models
+{
+    public class AppState 
+    {
+        public string BackgroundColor { get; private set; }
+
+        public event EventHandler OnBackgroundColorChange = delegate {};
+
+        public void BackgroundColorChange(string backgroundColor)
+        {
+            BackgroundColor = backgroundColor;
+            OnBackgroundColorChange(this , EventArgs.Empty);
+   
+        }
+
+  
+    }
+}
