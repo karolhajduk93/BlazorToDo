5ba00b3cacdd3185dba1331807cfec705b8a50c1
Karol Hajduk <Karol.hajduk@sabre.com>
2021-08-20 15:22:11 +0200

    make round checkbox with check sign

--- Models/TodoItemModel.cs
+++ Models/TodoItemModel.cs
@@ -3,12 +3,18 @@
     public class TodoItemModel
     {
         public string Name { get; set; }
+        public int Id { get; set; }
+
+        public bool Checked { get; set; } = false;
+
+        private static int  _idIncrementer = 0;
 
         public TodoItemModel() { }
 
         public TodoItemModel(string name)
         {
             Name = name;
+            Id = _idIncrementer++;
         }
     }
 }
