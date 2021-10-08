4b8b1f2a95b644b1bdfc515c0f8651507966aed6
Karol Hajduk <Karol.hajduk@sabre.com>
2021-10-08 17:43:12 +0200

    autoresize textarea and prevent enter input using interop

---
+++ wwwroot/js/interop.js
@@ -0,0 +1,7 @@
+﻿window.methods = {
+    resize: function(elementId) {
+        const myTextArea = document.getElementById(elementId);
+        myTextArea.style.height = 'auto';
+        myTextArea.style.height = (myTextArea.scrollHeight) + 'px';
+    }
+}
