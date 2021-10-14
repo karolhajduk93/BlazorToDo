2cac686e7d1aebf77f0e90944595a697db081c3d
Karol Hajduk <Karol.hajduk@sabre.com>
2021-10-14 18:09:33 +0200

    resize textarea on window resize

--- Models/TodoItemModel.cs
+++ Models/TodoItemModel.cs
@@ -5,23 +5,15 @@ namespace BlazorToDoApp.Models
     public class TodoItemModel
     {
         public string Text { get; set; }
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Date { get; set; }
-        public bool Checked { get; set; } 
-        public int IdCompleted { get; set; }
-
-
-        public static int  IdIncrementer = 0;
-        public static int  IdCompletedIncrementer = 0;
-
-        public TodoItemModel() { }
+        public bool Checked { get; set; }
 
         public TodoItemModel(string text)
         {
             Text = text;
-            Id = IdIncrementer++;
+            Id = Guid.NewGuid().ToString();
             Date = DateTime.Now.ToString("dd-MM-yyyy");
-            IdCompleted = -1;
         }
     }
 }
--- wwwroot/js/interop.js
+++ wwwroot/js/interop.js
@@ -3,5 +3,11 @@
         const myTextArea = document.getElementById(elementId);
         myTextArea.style.height = 'auto';
         myTextArea.style.height = (myTextArea.scrollHeight) + 'px';
+    },
+    customResize: function (elementId) {
+        DotNet.invokeMethodAsync('BlazorToDoApp', 'ResizedWindow')
+    },
+    onLoad: function () {
+        window.onresize = window.methods.customResize;
     }
 }
