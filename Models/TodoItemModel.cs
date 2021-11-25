8505a746d22fcb0c4f2f5c39da239e5fde65049e
Hajduk, Karol <karol.hajduk@sabre.com>
2021-11-25 16:13:17 +0100

    id name change

--- Models/TodoItemModel.cs
+++ Models/TodoItemModel.cs
@@ -5,14 +5,14 @@ namespace BlazorToDoApp.Models
     public class TodoItemModel
     {
         public string Text { get; set; }
-        public string Id { get; }
-        public string Date { get; }
+        public string ToDoItemId { get; set; }
+        public string Date { get; set; }
         public bool Checked { get; set; }
 
         public TodoItemModel(string text)
         {
             Text = text;
-            Id = Guid.NewGuid().ToString();
+            ToDoItemId = Guid.NewGuid().ToString();
             Date = DateTime.Now.ToString("dd-MM-yyyy");
         }
     }
