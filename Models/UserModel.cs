34c748d992e8f9e2f35a26442ec4d9ec847f2d6f
Hajduk, Karol <karol.hajduk.ctr@sabre.com>
2022-05-10 17:57:54 +0200

    Login method added

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -9,9 +9,13 @@ namespace BlazorToDoApp.Models
 	{
 		public string UserId { get; set; }
 
-		public string UserName { get; set; } = string.Empty;
+		public string Username { get; set; } = string.Empty;
 
 		public string Password { get; set; } = string.Empty;
+
+		public byte[] PasswordHash { get; set; }
+
+		public byte[] PasswordSalt { get; set; }
 
 		public string Email { get; set; } = string.Empty;
 
